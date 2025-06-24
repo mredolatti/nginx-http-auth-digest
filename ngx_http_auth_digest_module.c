7f8c8ab1bfb4d156f42c93695c85fceae8fdde1b
Martin Redolatti <martinredolatti@gmail.com>
2025-06-24 15:32:36 -0300

    remove extra whitespace

--- ngx_http_auth_digest_module.c
+++ ngx_http_auth_digest_module.c
@@ -870,7 +870,7 @@ ngx_http_auth_digest_verify_hash(ngx_http_request_t *r,
     // Set the stale value to 1 because the nonce value was not found in 
     // the digest tree, but the computation is valid.
     fields->stale = 1;
-   
+  
   invalid:
     // nonce is invalid/expired or client reused an nc value. suspicious...
     ngx_shmtx_unlock(&shpool->mutex);
